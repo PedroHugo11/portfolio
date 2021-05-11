5a53a59257320fa119da7291dc6503ed1f4db7cb
PedroHugo11 <pedrohuugoo11@hotmail.com>
2021-05-11 16:30:57 -0300

    Updates

---
+++ precache-manifest.ce84b64214cf5bed1dc707645ba999ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c87887c40823572127b9bb0958b111c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5c365165a27e22056fe",
+    "url": "./static/js/2.c5be6077.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.c5be6077.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cebb572d6322f65dff65",
+    "url": "./static/js/main.fef658ee.chunk.js"
+  },
+  {
+    "revision": "610a38b7b5a7cb7b2d5e",
+    "url": "./static/js/runtime-main.916853f0.js"
+  }
+]);
